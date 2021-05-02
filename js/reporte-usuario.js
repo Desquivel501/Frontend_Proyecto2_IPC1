a96100ffca45f800a88107e8719f4ffd66128c8c
Desquivel501 <3523114870101@ingenieria.usac.edu.gt>
2021-05-01 20:32:57 -0600

    Update reporte-usuario.js

--- js/reporte-usuario.js
+++ js/reporte-usuario.js
@@ -167,12 +167,12 @@ function generar_reporte(){
                 </html>
                                 `
             
-                                html2pdf(element, {
+                                html2pdf(plantilla, {
                                     margin:       10,
                                     filename:     'myfile.pdf',
                                     image:        { type: 'jpeg', quality: 0.98 },
                                     html2canvas:  { scale: 2, logging: true, dpi: 192, letterRendering: true },
-                                    jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
+                                    jsPDF:        { unit: 'mm', format: 'a4', orientation: 'lanscape' }
                                   });
                     
             })
